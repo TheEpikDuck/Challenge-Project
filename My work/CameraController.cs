ca62409760ff8b1b7ca42012d2d0ed513147010a
TheEpikDuck <114428976+TheEpikDuck@users.noreply.github.com>
2024-05-14 12:26:04 -0400

    Add files via upload

---
+++ My work/CameraController.cs
@@ -0,0 +1,46 @@
+//camera controller for the minigame in world 1-2, i wanted to use the code in sidescrolling but it would've messed up the other levels
+//I think i could've just used lateupdate for all of this tho... :/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float offset;
+    public float offsetSmoothing;
+    private Vector3 playerPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        if(player.transform.localScale.x >0f)
+        {
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        }
+
+        if(player.transform.localScale.y >0f)
+        {
+            playerPosition = new Vector3(playerPosition.x, playerPosition.y + offset, playerPosition.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x, playerPosition.y - offset, playerPosition.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+}
---
+++ My work/CharacterSelector.cs
@@ -0,0 +1,47 @@
+//this currently doesn't work but it's supposed to be the skin selector function
+//used a tutorial for this
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public GameObject[] skins;
+    public int selectedCharacter;
+    
+    private void Awake()
+    {
+        selectedCharacter = PlayerPrefs.GetInt("SelectedCharacter, 0");
+        foreach(GameObject player in skins)
+            player.SetActive(false);
+        
+        skins[selectedCharacter].SetActive(true);
+    }
+
+     public void ChangeNext()
+    {
+        skins[selectedCharacter].SetActive(false);
+        selectedCharacter++;
+        if(selectedCharacter == skins.Length);
+            selectedCharacter = 0;
+           
+        skins[selectedCharacter].SetActive(true);
+        PlayerPrefs.SetInt("SelectedCharacter", selectedCharacter);
+
+    }
+
+    public void ChangePrevious()
+    {
+        skins[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if(selectedCharacter == -1);
+            selectedCharacter = skins.Length -1;
+           
+        skins[selectedCharacter].SetActive(true);
+        PlayerPrefs.SetInt("SelectedCharacter", selectedCharacter);
+
+    }
+
+   
+}
